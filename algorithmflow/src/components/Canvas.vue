3f13dc167c8836b78a8fc291f8546c1182ba9360
819907785@qq.com <819907785@qq.com>
2022-08-11 14:54:46 +0800

    import rappid.css 添加画纸

--- algorithmflow/src/components/Canvas.vue
+++ algorithmflow/src/components/Canvas.vue
@@ -10,11 +10,52 @@
 <script>
 import * as joint from '../../build/package/rappid'
 export default {
-    mouted(){
+    mounted() {
         this.init()
     },
-    init(){
-        alert('输出一句话')
+    methods: {
+        init() {
+            //joint.setTheme('dark')
+            //joint.setTheme('material')
+            joint.setTheme('modern')
+            //joint.setTheme('default')
+            let graph = new joint.dia.Graph({},{cellNamespace:joint.shape})
+            let paper = new joint.dia.Paper({
+                width: 1000,
+                height: 1000,
+                gridSize: 10,
+                drawGrid: true,
+                model: graph, 
+                defaultLink: this.defalutLink,
+                interactive: {
+                    linkMove: false
+                },
+                snapLinks: {
+                    radius: 70
+                },
+                defaultConnectionPoint: {
+                    name: 'boundary'
+                },
+                cellViewNamespace: joint.shape
+            })
+
+            let paperScroller = new joint.ui.PaperScroller({
+                paper: paper,
+                autoResizePaper: true,
+                cursor: 'grab'
+            })
+            document.querySelector('.paper-container').appendChild(paperScroller.el)
+            paperScroller.render().center()
+        },  
+        defalutLink(elementView, magnet){
+            return new joint.shapes.standard.Link({
+                attrs: {
+                    line: {
+                        stroke: 'white'
+                    }
+                }
+            })
+        }
     }
 }
 </script>
--- algorithmflow/src/main.js
+++ algorithmflow/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import './assets/css/style.css'
+import '../build/package/rappid.css'
 
 createApp(App)
     .use(router)
