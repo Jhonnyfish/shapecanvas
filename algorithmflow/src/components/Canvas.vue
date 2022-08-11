80d81deda1db23db977e39ec96cc57d0c43fd404
819907785@qq.com <819907785@qq.com>
2022-08-11 13:47:08 +0800

    script 引入 rappid 并设置babel

--- algorithmflow/src/components/Canvas.vue
+++ algorithmflow/src/components/Canvas.vue
@@ -1,8 +1,20 @@
 <template>
 <div>
-    <div class="toolbar-container"></div>
-    <div class="stencil-container"></div>
-    <div class="paper-container"></div>
-    <div class="inspector-container"></div>
+    <div class="toolbar-container" ref="toolbar-container">工具条</div>
+    <div class="stencil-container" ref="stencil-container">模板</div>
+    <div class="paper-container" ref="paper-container">画纸</div>
+    <div class="inspector-container" ref="inspector-container">监视器</div>
 </div>
 </template>
+
+<script>
+import * as joint from '../../build/package/rappid'
+export default {
+    mouted(){
+        this.init()
+    },
+    init(){
+        alert('输出一句话')
+    }
+}
+</script>
