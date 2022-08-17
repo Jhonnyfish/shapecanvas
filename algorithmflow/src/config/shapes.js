b574ac5c7046727bd8a82cd8b06bd7c14c27f9a7
syq0404 <894326915@qq.com>
2022-08-17 14:11:27 +0800

    添加一个特殊的形状工具

--- algorithmflow/src/config/shapes.js
+++ algorithmflow/src/config/shapes.js
@@ -166,6 +166,143 @@ export function defineMyShape(){
             selector: 'label'
         }]
     })
+    joint.dia.Element.define('myApp.Question', {
+            optionHeight: 30,
+            questionHeight: 45,
+            paddingBottom: 30,
+            minWidth: 150,
+            question: null,
+            ports: {
+                groups: {
+                    'in': {
+                        position: 'top',
+                        attrs: {
+                            circle: {
+                                magnet: 'passive',
+                                stroke: 'white',
+                                fill: '#feb663',
+                                r: 14
+                            },
+                            text: {
+                                pointerEvents: 'none',
+                                fontSize: 12,
+                                fill: 'white'
+                            }
+                        },
+                        label: {
+                            position: {
+                                name: 'left',
+                                args: { x: 5 }
+                            }
+                        }
+                    },
+                    out: {
+                        position: 'right',
+                        attrs: {
+                            'circle': {
+                                magnet: true,
+                                stroke: 'none',
+                                fill: '#31d0c6',
+                                r: 10
+                            }
+                        }
+                    }
+                },
+                items: [{
+                    group: 'in',
+                    attrs: {
+                        text: { text: 'in' }
+                    }
+                },{
+                    group: 'out',
+                    
+                },
+                {
+                    group: 'out',
+                   
+                },
+                {
+                    group: 'out',
+                   
+                }]
+            },
+            attrs: {
+                '.': {
+                    magnet: false
+                },
+                '.body': {
+                    refWidth: '100%',
+                    refHeight: '100%',
+                    rx: '1%',
+                    ry: '2%',
+                    stroke: 'none',
+                    fill: {
+                        type: 'linearGradient',
+                        stops: [
+                            { offset: '0%', color: '#FEB663' },
+                            { offset: '100%', color: '#31D0C6' }
+                        ],
+                        // Top-to-bottom gradient.
+                        attrs: { x1: '0%', y1: '0%', x2: '0%', y2: '100%' }
+                    }
+                },
+                '.btn-add-option': {
+                    refX: 10,
+                    refDy: -22,
+                    cursor: 'pointer',
+                    fill: 'white'
+                },
+                '.btn-remove-option': {
+                    xAlignment: 10,
+                    yAlignment: 13,
+                    cursor: 'pointer',
+                    fill: 'white'
+                },
+                '.options': {
+                    refX: 0
+                },
+    
+                // Text styling.
+                text: {
+                    fontFamily: 'Arial'
+                },
+                '.option-text': {
+                    fontSize: 11,
+                    fill: '#4b4a67',
+                    refX: 30,
+                    yAlignment: 'middle'
+                },
+                '.question-text': {
+                    fill: 'white',
+                    refX: '50%',
+                    refY: 15,
+                    fontSize: 15,
+                    textAnchor: 'middle',
+                    style: {
+                        textShadow: '1px 1px 0px gray'
+                    }
+                },
+    
+                // Options styling.
+                '.option-rect': {
+                    rx: 3,
+                    ry: 3,
+                    stroke: 'white',
+                    strokeWidth: 1,
+                    strokeOpacity: .5,
+                    fillOpacity: .5,
+                    fill: 'white',
+                    refWidth: '100%'
+                }
+            }
+        },
+        {
+    
+        markup: '<rect class="body"/><text class="question-text"/><g class="options"></g>',
+        // optionMarkup: '<g class="option"><rect class="option-rect"/><path class="btn-remove-option" d="M0,0 15,0 15,5 0,5z"/><text class="option-text"/></g>',
+    
+        
+    });
 }
 
 export function createMyShape(){
@@ -175,5 +312,11 @@ export function createMyShape(){
         attrs: { label: { text: 'My Shape' }},
         level: 2,
         ports: { items: [{ id: 'in1', group: 'in' }, { group: 'out', id: 'out1' }] }
+    });
+}
+export function createMyShape2(){
+    return new joint.shapes.myApp.Question({
+        position: { x: 400 - 50, y: 30 },
+        size: { width: 100, height: 70 },
     });
 }
--- algorithmflow/src/config/stencil.js
+++ algorithmflow/src/config/stencil.js
@@ -1,5 +1,5 @@
 import { createModel } from "./shapes";
-
+import { createMyShape2 } from "./shapes";
 export function createStencil(paperScroller){
     var stencil = new joint.ui.Stencil({
         paper: paperScroller,
@@ -7,7 +7,8 @@ export function createStencil(paperScroller){
         width: 240,
         groups: {
             myShapesGroup1: { index: 1, label: ' 基础形状' },
-            myShapesGroup2: { index: 2, label: ' 输入输出' }
+            myShapesGroup2: { index: 2, label: ' 输入输出' },
+            myShapesGroup3: { index: 3, label: ' 特殊形状' }
         },
         dropAnimation: true, //拖拽时的样式
         groupsToggleButtons: true, //groups全部展开
@@ -19,6 +20,7 @@ export function createStencil(paperScroller){
 
     document.querySelector('.stencil-container').appendChild(stencil.el);
     var model = createModel()
+    var model2 = createMyShape2()
     stencil.render().load({
         myShapesGroup1: [{
             type: 'standard.Rectangle',
@@ -34,9 +36,10 @@ export function createStencil(paperScroller){
                 { group: 'in' },
                 { group: 'out' }, 
                 { group: 'out' }
-            ],
-        },
+            ],          
+        },      
         size: {  width:200,height: 150 },
-        }]
+        }],
+        myShapesGroup3: [model2]
     });
 }
