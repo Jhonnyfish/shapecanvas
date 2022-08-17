dae0737556e358049fd25132551e031afeb84213
819907785@qq.com <819907785@qq.com>
2022-08-17 15:42:06 +0800

    删除路由，解决npm build 主页空白问题

--- algorithmflow/src/App.vue
+++ algorithmflow/src/App.vue
@@ -1,7 +1,32 @@
 <template>
-<div id=app>
-  <router-view></router-view>
+<div id="app">
+    <div class="toolbar-container"></div>
+    <div class="stencil-container"></div>
+    <div class="paper-container"></div>
+    <div class="inspector-container"></div>
 </div>
 </template>
-<style>
-</style>
+
+<script>
+import {init} from './config/common.js'
+import * as joint from './../build/package/rappid.js'
+window.joint = joint
+export default {
+    data() {
+        return {
+            joint : joint
+        }
+    },
+    mounted() {
+        // joint.setTheme('modern')
+        joint.setTheme('dark');
+        //joint.setTheme('material');
+        //joint.setTheme('default');
+        init(this.joint)
+    },
+    methods: {
+
+    }
+
+}
+</script>
--- algorithmflow/src/main.js
+++ algorithmflow/src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
 import './assets/css/style.css'
 import '../build/package/rappid.css'
 
 createApp(App)
-    .use(router)
     .mount('#app')
